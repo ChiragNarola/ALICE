75e624d4425b8d8a9080c76f39e3dbedf2fb9938
Nikhil79922 <ns94301918@gmail.com>
2025-08-26 17:15:48 +0530

    added validation in middle name

--- src/components/Step1ChildInfo.tsx
+++ src/components/Step1ChildInfo.tsx
@@ -11,7 +11,7 @@ import { toast } from 'react-toastify';
 const childSchema = z.object({
   id: z.union([z.string(), z.number()]).optional(),
   firstName: z.string().min(2, "First name is required"),
-  middleName: z.string().optional(),
+  middleName: z.string().min(2, "Middle name is required"),
   lastName: z.string().min(2, "Last name is required"),
   gender: z.enum(['Boy', 'Girl', 'Prefer not to say']),
   dob: z.string().min(1, "Date of Birth is required"),
@@ -227,7 +227,7 @@ function handleDelete(idx: number, child_id: string | number | undefined) {
             {/* Middle Name */}
             <div className="flex-1 min-w-[180px]">
               <label className="block text-left text-[14px] lg:text-base font-semibold text-alice-black relative ms-[12px] mt-[2px]">
-                <span className="bg-[#FEFCF8] px-[5px]">Child’s Middle Name</span>
+                <span className="bg-[#FEFCF8] px-[5px]">Child’s Middle Name <span className="text-red-500">*</span></span>
               </label>
               <Controller
                 name={`children.${idx}.middleName`}
